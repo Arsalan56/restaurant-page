b192b400719e8a12fda1483868171bf4ce6491ea
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-07-11 10:36:26 -0400

    Create all dom elements manually in js
    The dom elements are created in the javascript side, but are not fully
    modified yet.

---
+++ src/menu.js
@@ -0,0 +1,47 @@
+export default () => {
+    const body = document.querySelector('body');
+    const main = document.createElement('main');
+    const title = document.createElement('p');
+
+    const imgcont1 = document.createElement('div');
+    const imgcont2 = document.createElement('div');
+    const imgcont3 = document.createElement('div');
+    const imgcont4 = document.createElement('div');
+    const imgcont5 = document.createElement('div');
+    const imgcont6 = document.createElement('div');
+
+    const img1src = document.createElement('p');
+    const img2src = document.createElement('p');
+    const img3src = document.createElement('p');
+    const img4src = document.createElement('p');
+    const img5src = document.createElement('p');
+    const img6src = document.createElement('p');
+
+    const img1anc = document.createElement('a');
+    const img2anc = document.createElement('a');
+    const img3anc = document.createElement('a');
+    const img4anc = document.createElement('a');
+    const img5anc = document.createElement('a');
+    const img6anc = document.createElement('a');
+
+    const img1 = document.createElement('img');
+    const img2 = document.createElement('img');
+    const img3 = document.createElement('img');
+    const img4 = document.createElement('img');
+    const img5 = document.createElement('img');
+    const img6 = document.createElement('img');
+
+    const img1name = document.createElement('p');
+    const img2name = document.createElement('p');
+    const img3name = document.createElement('p');
+    const img4name = document.createElement('p');
+    const img5name = document.createElement('p');
+    const img6name = document.createElement('p');
+
+    const img1cost = document.createElement('p');
+    const img2cost = document.createElement('p');
+    const img3cost = document.createElement('p');
+    const img4cost = document.createElement('p');
+    const img5cost = document.createElement('p');
+    const img6cost = document.createElement('p');
+};
