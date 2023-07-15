a3ab30ff5d243d57f6446139c3118a5c472fd31b
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-07-15 10:30:28 -0400

    Allow the user to switch between pages with js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,49 @@
 import CreateHome from './home';
 import CreateMenu from './menu';
 import CreateContact from './contact';
-// CreateHome();
+
 // CreateMenu();
-CreateContact();
+// CreateContact();
+
+// function menu() {}
+((Home, Menu, Contact) => {
+    Home();
+    const tabs = document.querySelectorAll('header li');
+
+    const logo = document.querySelector('.h-top img:first-of-type');
+
+    function Remove() {
+        const body = document.querySelector('body');
+        const main = document.querySelector('main');
+        body.removeChild(main);
+    }
+
+    const btnReload = () => {
+        const menubtn = document.querySelector('.home > button');
+        menubtn.addEventListener('click', () => {
+            Remove();
+            Menu();
+        });
+    };
+
+    tabs[0].addEventListener('click', () => {
+        Remove();
+        Home();
+        btnReload();
+    });
+    logo.addEventListener('click', () => {
+        Remove();
+        Home();
+        btnReload();
+    });
+
+    tabs[1].addEventListener('click', () => {
+        Remove();
+        Menu();
+    });
+    btnReload();
+    tabs[2].addEventListener('click', () => {
+        Remove();
+        Contact();
+    });
+})(CreateHome, CreateMenu, CreateContact);
